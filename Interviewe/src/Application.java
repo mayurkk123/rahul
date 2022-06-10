97b48467564596091c70d264a947b31051b01357
Admin <Admin@DESKTOP-5V6VI5K>
2022-06-10 13:51:15 +0530

    First Commit

---
+++ Interviewe/src/Application.java
@@ -0,0 +1,12 @@
+
+public class Application extends Student1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student1 obj = new Student1();
+		obj.Name3("Mayur", "Keskar", 10);
+		obj.Name2("rahul", "kulkarni", 20);
+		obj.Name2("kedar", "khan", 30);
+	}
+
+}
---
+++ Interviewe/src/Student1.java
@@ -0,0 +1,36 @@
+
+public class Student1 {
+
+	
+	public void Name2(String f, String l, int n ) {
+	String firstName = "Suhas";
+	System.out.println("First name"+ f );
+	System.out.println( "Last name"+ l );
+	System.out.println("Rollnumber"+n );
+	
+	
+	}
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+	}
+
+	public void Name3(String f, String l, int n) {
+		// TODO Auto-generated method stub
+		String firstName = "Suhas";
+		System.out.println("First name"+ f );
+		System.out.println( "Last name"+ l );
+		System.out.println("Rollnumber"+n );
+		
+		
+	}
+
+	public void Name4(String f, String l, int n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
